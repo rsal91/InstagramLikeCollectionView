8aaf1f0b6ad847a43fc352eb328353e2b408bcdf
Roman Salazar <romansito18@gmail.com>
2016-09-22 12:11:13 -0700

    Created UICollectionviewFlowLayout

---
+++ GridCollectionView/CustomFlowLayout.swift
@@ -0,0 +1,22 @@
+//
+//  CustomFlowLayout.swift
+//  GridCollectionView
+//
+//  Created by Roman Salazar on 9/22/16.
+//  Copyright © 2016 Roman Salazar. All rights reserved.
+//
+
+import UIKit
+
+class CustomFlowLayout: UICollectionViewFlowLayout {
+
+    override init() {
+        super.init()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
